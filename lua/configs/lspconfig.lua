e8b7fc079768f9d3e7d1a9fb53f5a5e4b243221b
derangga <derangga1011@gmail.com>
2025-10-13 22:58:34 +0700

    update lspconfig

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,49 +3,48 @@ require("nvchad.configs.lspconfig").defaults()
 local vue_language_server = vim.fn.expand(vim.fn.stdpath "data" ..
     "/mason/packages/vue-language-server/node_modules/@vue/language-server")
 local servers = {
-    "bashls",
-    "clangd",
-    "cssls",
-    "gopls",
-    "html",
-    "jsonls",
-    "lua_ls",
-    "marksman",
-    "sqls",
-    "tailwindcss",
-    "ts_ls",
-    "vue_ls"
-}
-
-vim.lsp.config("vue_ls", {
-    init_options = {
-        vue = {
-            hybridMode = false, -- Recommended to turn off hybridMode
-        },
-    },
-    filetypes = { "vue" },
-})
-
-vim.lsp.config("ts_ls", {
-    init_options = {
-        plugins = {
-            {
-                name = "@vue/typescript-plugin",
-                location = vue_language_server,
-                languages = { "vue", "javascript", "typescript" },
+    bashls = {},
+    clangd = {},
+    cssls = {},
+    gopls = {},
+    html = {},
+    jsonls = {},
+    lua_ls = {},
+    marksman = {},
+    sqls = {},
+    tailwindcss = {},
+    ts_ls = {
+        init_options = {
+            plugins = {
+                {
+                    name = "@vue/typescript-plugin",
+                    location = vue_language_server,
+                    languages = { "vue", "javascript", "typescript" },
+                },
             },
         },
+        filetypes = {
+            "typescript",
+            "javascript",
+            "javascriptreact",
+            "typescriptreact",
+            "vue",
+        },
     },
-    filetypes = {
-        "typescript",
-        "javascript",
-        "javascriptreact",
-        "typescriptreact",
-        "vue",
+    vue_ls = {
+        init_options = {
+            vue = {
+                hybridMode = false, -- Recommended to turn off hybridMode
+            },
+        },
+        filetypes = { "vue" },
     },
-})
+}
 
-vim.lsp.enable(servers)
+for name, opts in pairs(servers) do
+  vim.lsp.config(name, opts)
+  vim.lsp.enable(name)
+end
 
 -- https://github.com/naborisk/dotfiles/blob/383041e06c070d78e4d990b662cfa13d35ce0a64/nvim/after/plugin/nvim-lspconfig.lua#L158
 vim.diagnostic.config {
