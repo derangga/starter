ef9f6215e6e783577763bc7fc6bb9597fe505487
Dimas Rangga <dimas.armando@sociolla.com>
2025-10-23 14:39:08 +0700

    update lsp key mapping to hover code

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,8 +6,11 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map("n", "K", vim.lsp.buf.hover, { desc = "Hover code" })
+
+-- LSP mapping
+map("n", "H", vim.lsp.buf.hover, { desc = "[H]over code" })
 map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Lsp [C]ode [A]ction" })
+
 map("n", "<leader>lg", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
